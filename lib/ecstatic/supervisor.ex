80c946a5c60de81cc738d71b8b2c717a993a4249
luisBlanco <luisb.mier@gmail.com>
2020-04-21 20:23:20 +0100

    just comments 4 tomorrow

--- lib/ecstatic/supervisor.ex
+++ lib/ecstatic/supervisor.ex
@@ -1,17 +1,21 @@
 defmodule Ecstatic.Supervisor do
+  require Logger
   @moduledoc false
   use Supervisor
 
   def start_link(arg \\ []) do
+    Logger.debug()
     Supervisor.start_link(__MODULE__, arg, name: __MODULE__)
   end
 
-  def init(_arg) do
+  def init(arg) do
     children = [
       {Ecstatic.Store.Ets, []},
       {Ecstatic.EventSource, []},
       {Ecstatic.EventProducer, []}
     ]
+    #lets put args somewhere useful. The watchers might be stored in the Ets or maybe in another genserver
+    #Ecstatic.Store.Watchers.get_watchers
 
     Supervisor.init(children, strategy: :one_for_one)
   end
